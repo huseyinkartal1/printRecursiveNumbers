3ac4237656970252513218653da3100fcff9b167
osoka46 <osman.bozdag@yahoo.com>
2022-03-12 11:46:48 +0100

    .

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    boolean isCheck(int number)
+    {
+        for (int x=1;x<number;x++)
+        {
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int right=3;
+       int []arr;
+
+
+        System.out.println("what size array do you want to creat?");
+
+        while (right>0)
+        {
+
+
+        
+
+        }
+
+
+
+
+
+    }
+}
